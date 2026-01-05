dda64adc91d956b841bbebf819a801c18bfa97e1
endhel <endhel.andradej@gmail.com>
2026-01-05 16:19:51 -0300

    solucao do vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // GARANTA que está assim, ou remova essa linha (o padrão já é /)
+})
