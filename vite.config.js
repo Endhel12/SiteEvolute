6feef2b5787443f59f46d69a1ab155ac826f7058
endhel <endhel.andradej@gmail.com>
2026-01-05 17:08:39 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // GARANTA que está assim, ou remova essa linha (o padrão já é /)
+  base: '/SiteEvolute/',
 })
